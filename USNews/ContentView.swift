64ece4d371926555a89ef4d9afbf36e4a6110802
Vinayakam <vinayakyalapalli@gmail.com>
2025-03-23 16:14:01 +0530

    Initial Commit

---
+++ USNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  USNews
+//
+//  Created by Vinayakam Y on 23/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ USNews/USNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  USNewsApp.swift
+//  USNews
+//
+//  Created by Vinayakam Y on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct USNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
